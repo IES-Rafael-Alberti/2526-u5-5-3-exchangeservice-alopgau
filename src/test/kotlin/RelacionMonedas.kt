94213c49c51a8d8214123801ef8fc8b162bfa112
alopgau <alopezg@ieslabahia.es>
2026-04-20 15:23:11 +0200

    relacion monedas test

--- src/test/kotlin/RelacionMonedas.kt
+++ src/test/kotlin/RelacionMonedas.kt
@@ -1,10 +1,12 @@
 package com.example.exchange
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.verify
 import io.mockk.verifySequence
 import org.iesra.revilofe.ExchangeRateProvider
 import org.iesra.revilofe.ExchangeService
@@ -28,23 +30,27 @@ class RelacionMonedas: DescribeSpec({
             val service = ExchangeService(providerSpy)
 
             it("Origen es igual a destino") {
-
-
+                service.exchange(Money(1000, "USD"),"USD") shouldBe 1000
+                verify (exactly = 0) {providerSpy.rate(any())}
             }
 
 
             it("Origen distinto de destino (tasa directa)") {
+                service.exchange(Money(1000, "USD"),"EUR") shouldBe 920
+                verify (exactly = 1) {providerSpy.rate(any())}
 
 
             }
 
             it("Origen distinto de destino (ruta cruzada)") {
-
-
+                service.exchange(Money(1000, "USD"),"JPY") shouldBe 605.9
+                verify (exactly = 2) {providerSpy.rate(any())}
 
             }
-
             it("Conversion imposible") {
+                shouldThrow<IllegalArgumentException> {
+                service.exchange(Money(1000, "EUR"),"JPY")
+                }
 
             }
 
